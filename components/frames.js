4e7acf78c677161b87c546da0bb8c294137a59ea
Johnmar-LCJS <74068966+Johnmar-LCJS@users.noreply.github.com>
2022-03-04 11:40:55 +0800

    Test

---
+++ components/frames.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import styles from '../styles/frames.module.css'
+
+function frames() {
+	return <div className={styles.frames}></div>
+}
+
+export default frames
