d76ee3e39792e4bc6b66bcc18fe0b8f79f1eb74a
Johnmar-LCJS <74068966+Johnmar-LCJS@users.noreply.github.com>
2022-03-06 10:44:42 +0800

    update

--- components/button.js
+++ components/button.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
 
 const button = ({ toggle }) => {
 	return (
-		<motion.button onClick={toggle} whileTap={{ scale: '0.9' }} style={{ background: 'transparent', border: 'none', padding: '1rem' }}>
+		<button onClick={toggle} whileTap={{ scale: '0.9' }} style={{ background: 'transparent', border: 'none', padding: '1rem' }}>
 			<Image src='/menuIcon.svg' alt='menu' width={50} height={50} />
-		</motion.button>
+		</button>
 	)
 }
 
--- components/sidemenu.js
+++ components/sidemenu.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import styles from '../styles/Sidemenu.module.css'
-import { motion } from 'framer-motion'
 import Menu from './Menuarray'
 
 const Sidemenu = () => {
 	return (
-		<motion.div className={styles.menuContainer}>
+		<div className={styles.menuContainer}>
 			<Menu />
-		</motion.div>
+		</div>
 	)
 }
 
